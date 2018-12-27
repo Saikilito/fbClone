3309c3557274d19e9e47cb2f7ee5872f3fbe8ed4
Saikilito <kembert@gmail.com>
2018-12-27 17:21:49 -0400

    Initial Commit

---
+++ js/myJs.js
@@ -0,0 +1,16 @@
+$(function(){
+    var boton = $('#btn-menu');
+    var fondo_enlace = $('#fondo-enlace');
+
+    boton.on('click', function(e){
+        fondo_enlace.toggleClass('active');
+        $('#barra-izquierda').toggleClass('active');
+        e.prevenDefault();
+    });
+
+    fondo_enlace.on('click', function(e){
+        fondo_enlace.toggleClass('active');
+        $('#barra-izquierda').toggleClass('active');
+        e.prevenDefault();
+    })
+}())
